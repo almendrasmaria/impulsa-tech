628f523119610c8e9bae7330c9f9eaa84a489262
maria <almendrasmaria04@gmail.com>
2025-10-24 14:28:40 -0300

    Agregar animaciones al componente About utilizando framer-motion

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,14 @@
+import { motion } from "framer-motion";
+
 const About = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden">
+    <motion.div
+      initial={{ opacity: 0, x: 200 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: false }}
+      className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+    >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         Sobre <span className="underline underline-offset-4 decoration-1 font-light"> ImpulsaTech</span>
       </h1>
@@ -36,7 +43,7 @@ const About = () => {
           <button className="bg-blue-600 text-white px-8 py-2 rounded hover:bg-blue-700 transition-colors">Ver oportunidades</button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
