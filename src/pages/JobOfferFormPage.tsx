a6a71eb43d955ea9d5e90660c42b47519c188457
luciaviazzo <viazzo94@gmail.com>
2025-10-27 00:43:39 -0300

    Agregar páginas y rutas para navegación como empresa

---
+++ src/pages/JobOfferFormPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import JobOfferForm from '../components/JobOfferForm';
+
+const JobOfferFormPage: React.FC = () => {
+    return <JobOfferForm />;
+};
+
+export default JobOfferFormPage;
---
+++ src/pages/JobOffersTablePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobOffersTable from '../components/JobOffersTable';
+
+const JobOffersTablePage: React.FC = () => {
+        return (
+            <div className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden bg-gray-100 min-h-screen flex items-center justify-center">
+                <JobOffersTable />
+            </div>
+        );
+};
+
+export default JobOffersTablePage;
