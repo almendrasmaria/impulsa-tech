8d33cf0184d98f1ca32f7a6d8130f6c875303ed6
maria <almendrasmaria04@gmail.com>
2025-11-07 20:29:34 -0300

    fix: ajustar el estilo del contenedor en la página de oportunidades

--- src/pages/OpportunitiesPage.tsx
+++ src/pages/OpportunitiesPage.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components";
 
 const OpportunitiesPage = () => {
   return (
-    <div className="w-full overflow-hidden">
+    <div className="min-h screen bg-[#F8F9FC]">
       <Navbar variant="light" />
     </div>
   );
